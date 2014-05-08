d89af771242fde2b5d4166751a5b5c9075873d31
Zac Clay <zacclay@gmail.com>
2014-05-08 15:34:55 -0400

    Finished level 4

--- player.rb
+++ player.rb
@@ -1,9 +1,21 @@
 class Player
+
   def play_turn(warrior)
-    if warrior.health < 15 and warrior.feel.empty?
+    @current_health = warrior.health
+    if should_rest?(warrior)
       warrior.rest!
     else
       warrior.feel.empty? ? warrior.walk! : warrior.attack!
+      @start_of_turn_health = warrior.health
     end
   end
+
+  def under_attack?(start_of_turn_health, current_health)
+    true unless start_of_turn_health <= current_health
+  end
+
+  def should_rest?(warrior)
+    warrior.health < 15 and warrior.feel.empty? and not under_attack?(@start_of_turn_health, @current_health)
+  end
+
 end

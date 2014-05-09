d5e05df06d3ebfc2509f92e1d87ef8f6186ec0d9
Zac Clay <zacclay@gmail.com>
2014-05-09 09:28:13 -0400

    Finished level 8

--- player.rb
+++ player.rb
@@ -6,13 +6,11 @@ class Player
   end
 
   def play_turn(warrior)
-    puts @direction
-    puts should_go_backwards?
     @current_health = warrior.health
     if should_rest?(warrior)
       warrior.rest!
     else
-      take_action(warrior)
+      what_to_do?(warrior, warrior.look)
       @start_of_turn_health = warrior.health
     end
   end
@@ -54,6 +52,30 @@ class Player
       warrior.walk!(@direction)
     else
       attack_or_rescue(warrior)
+    end
+  end
+
+  def closest_thing(array_of_spaces)
+    array_of_spaces.each do |space|
+      unless space.empty?
+        return [space]
+        break
+      end
+    end
+  end
+
+  def enemy_is_closest?(array_of_spaces)
+    closest_thing(array_of_spaces).any? {|space| space.enemy?}
+  end
+
+  def what_to_do?(warrior, array_of_spaces)
+    right_in_front = array_of_spaces[0]
+    if right_in_front.captive?
+      warrior.rescue!(@direction)
+    elsif right_in_front.empty? && !enemy_is_closest?(array_of_spaces)
+      warrior.walk!(@direction)
+    elsif enemy_is_closest?(array_of_spaces)
+      warrior.shoot!
     end
   end
 
